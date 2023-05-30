dd7ff0c94c19104ebfc3acbe6aa0c9bac5fc67ef
Justinnirvan04 <nirvantiojustin@gmail.com>
2023-05-31 00:42:04 +0700

    initial commit

---
+++ app/src/main/java/com/just/projectakhirpbb/api/Apiurl.java
@@ -0,0 +1,6 @@
+package com.just.projectakhirpbb.api;
+
+public class Apiurl {
+    public static final String BASE_URL = "http://localhost/restjust/api/public/absensi/";
+    public static final String API_KEY = "294dcca06bf811ed9ebd4cedfb0fa98d";
+}
